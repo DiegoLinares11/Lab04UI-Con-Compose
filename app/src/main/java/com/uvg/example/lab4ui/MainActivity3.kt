389ac673d520f64bfded37b71071cc5ef30aa4fe
DiegoLinares11 <lin221256@uvg.edu.gt>
2024-09-04 16:27:28 -0600

    Parte de arriba de la tercer pantalla

--- app/src/main/java/com/uvg/example/lab4ui/MainActivity3.kt
+++ app/src/main/java/com/uvg/example/lab4ui/MainActivity3.kt
@@ -4,20 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.uvg.example.lab4ui.ui.theme.GrayColor
+import com.uvg.example.lab4ui.ui.theme.GreenColor
 import com.uvg.example.lab4ui.ui.theme.Lab4UITheme
 
 class MainActivity3 : ComponentActivity() {
@@ -40,29 +61,35 @@ fun settingsView(modifier: Modifier){
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun appTercerVista(){
+fun appTercerVista() {
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
                     Box(
-                        modifier = Modifier.fillMaxWidth(),
-                        contentAlignment = Alignment.Center
-                    ){
-                        Text(text = "Settings")
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.x),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(24.dp)
+                                .align(Alignment.CenterStart)
+                        )
+                        Text(
+                            text = "Settings",
+                            color = Color.Black,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
                     }
-
                 },
-                colors = topAppBarColors(
-                    containerColor = Color (0xFFFFFF),
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.White,
                     titleContentColor = Color.Black
                 )
-
             )
         }
-    ){
-        padding ->
+    ) { padding ->
         settingsView(modifier = Modifier.padding(padding))
-        
     }
 }
