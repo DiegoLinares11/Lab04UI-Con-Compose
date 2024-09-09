553b72d6d76b2c15810c343ff91b525cc153ea1a
Ctribsz <122636369+Ctribsz@users.noreply.github.com>
2024-09-09 03:14:15 -0600

    Pantalla 2 y 4 Chris

--- app/src/main/java/com/uvg/example/lab4ui/MainActivity.kt
+++ app/src/main/java/com/uvg/example/lab4ui/MainActivity.kt
@@ -4,13 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.uvg.example.lab4ui.ui.theme.GrayColor
+import com.uvg.example.lab4ui.ui.theme.GreenColor
 import com.uvg.example.lab4ui.ui.theme.Lab4UITheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +45,146 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab4UITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                appConCompose()
             }
         }
     }
 }
 
+
+//Esto es lo que se vera dentro de la aplicacion:
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun imagenesMargen(modifier: Modifier = Modifier){
+    Column(
         modifier = modifier
+            .fillMaxSize()
+            //.padding(16.dp)
+    ){
+        Image(
+            painter = painterResource(R.drawable.carrera_bi_e1692665091446),
+            contentDescription = "Imagen Principal",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        SectionTitle(title = stringResource(id = R.string.DESTACADOS))
+        //PRIMER COMPONENTE DE IMAGENES.
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ){
+            imagenesCards(
+                imageID = R.drawable.imagenes_galeria_cit_02,
+                title = stringResource(id = R.string.Service_now),
+                colorFondo = GreenColor, //Esto es para el verde Lo pase para Theme.kt para usarlos como constantes
+                modifier = Modifier.weight(1f)
+            )
+            imagenesCards(
+                imageID = R.drawable.zro07729,
+                title = stringResource(id = R.string.Actualidad_UVG) ,
+                colorFondo = GrayColor, //GRIS Lo pase para theme.kt para ser usados como constantes.
+                modifier = Modifier.weight(1f)
+            )
+        }
+        //ESTE ES EL SEGUNDO COMPONENTE DE IMAGENES
+        SectionTitle(title = stringResource(id = R.string.Servicios_Recursos))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ){
+            imagenesCards(
+                imageID = R.drawable.uvg2_1765825cf2,
+                title = stringResource(id = R.string.directorioEstudiantiles),
+                colorFondo = GreenColor,
+                modifier = Modifier.weight(1f))
+            imagenesCards(
+                imageID = R.drawable.portada_becas_carrera_uvg,
+                title = stringResource(id = R.string.PortalBiblioUVG),
+                colorFondo = GrayColor,
+                modifier = Modifier.weight(1f))
+        }
+
+
+    }
+
+}
+//Estas son como que la estructura de las imagenes que estaran dentro de cards
+@Composable
+fun imagenesCards(imageID: Int, title: String, colorFondo: Color, modifier: Modifier){
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        modifier = modifier
+    ){
+        Box(
+            modifier = Modifier
+                .height(150.dp)
+        ) {
+            Image(
+                painter = painterResource(id = imageID),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .background(colorFondo)
+                    .fillMaxWidth()
+                    .padding(6.dp)
+            ){
+                Text(
+                    text = title,
+                    color = Color.White
+                )
+
+            }
+
+        }
+
+    }
+}
+
+//Funcion solo para imprimir un titulo.
+//Pendiente de agregar tipografias
+@Composable
+fun SectionTitle(title: String) {
+    Text(
+        text = title,
+        style = MaterialTheme.typography.bodyLarge,
+        modifier = Modifier.padding(vertical = 8.dp),
+        color = Color.Gray
     )
 }
 
-@Preview(showBackground = true)
+//El scaffold define la plantilla de la aplicación está acá
+@OptIn(ExperimentalMaterial3Api::class)
+//@Preview
 @Composable
-fun GreetingPreview() {
-    Lab4UITheme {
-        Greeting("Android")
+fun appConCompose() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text(text = stringResource(id = R.string.Campus_Central))
+                    }
+                },
+                colors = topAppBarColors(
+                    containerColor = Color(0xFFFFFF),
+                    titleContentColor = Color.Black // Color del texto
+                )
+            )
+        }
+    ) { padding ->
+        imagenesMargen(modifier = Modifier.padding(padding))
     }
 }
+
